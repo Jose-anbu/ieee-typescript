db10f2d5ee519b13f0f8bf83655bbdf324a0c529
Jose Antonio Burgos <jose_7892@hotmail.com>
2023-06-18 20:43:44 -0300

    Tipos de datos

--- CLASE-01/PrimerPrograma/holaMundo1.ts
+++ CLASE-01/PrimerPrograma/holaMundo1.ts
@@ -4,3 +4,9 @@ console.log(mensaje);
 // Constante
 const mensaje2: string = "Hola Mundo 2!";
 console.log(mensaje2);
+
+const numero: number = 3.14;
+console.log(numero);
+
+const valorBooleano: boolean = true;
+console.log(valorBooleano);
