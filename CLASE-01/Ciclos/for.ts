f21849f899699c64c0be3ffd753e5b752d541252
Jose Antonio Burgos <jose_7892@hotmail.com>
2023-06-18 23:58:57 -0300

    Ejemplo de for con switch-case

--- CLASE-01/Ciclos/for.ts
+++ CLASE-01/Ciclos/for.ts
@@ -3,7 +3,7 @@
 }*/
 
 // Ejemplo usando for, if-else
-for (let diaDeSemana = 1; diaDeSemana <= 7; diaDeSemana++) {
+/*for (let diaDeSemana = 1; diaDeSemana <= 7; diaDeSemana++) {
     if (diaDeSemana === 1) {
         console.log("Hoy es Lunes");
     } else if (diaDeSemana === 2) {
@@ -18,5 +18,28 @@ for (let diaDeSemana = 1; diaDeSemana <= 7; diaDeSemana++) {
         console.log("Hoy es Sábado");
     } else {
         console.log("Hoy es Domingo");
+    }
+}*/
+
+// Ejemplo usando for y switch
+for (let diaDeSemana = 1; diaDeSemana <= 8; diaDeSemana++) {
+    switch (diaDeSemana) {
+        case 1: console.log("Hoy es Lunes");
+            break;
+        case 2: console.log("Hoy es Martes");
+            break;
+        case 3: console.log("Hoy es Miércoles");
+            break;
+        case 4: console.log("Hoy es Jueves");
+            break;
+        case 5: console.log("Hoy es Viernes");
+            break;
+        case 6: console.log("Hoy es Sábado");
+            break;
+        case 7: console.log("Hoy es Domingo");
+            break;
+        default:
+            console.log("Este no es un día de la semana correcto", diaDeSemana);
+            break;
     }
 }
