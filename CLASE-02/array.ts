1dc4b162bfc28472bc6ee62db1cc826919090d77
Jose Antonio Burgos <jose_7892@hotmail.com>
2023-06-19 03:12:58 -0300

    Metodos push y pop

--- CLASE-02/array.ts
+++ CLASE-02/array.ts
@@ -1,3 +1,11 @@
 let diasDeSemana: string[] = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
 
 console.log(diasDeSemana[0]);
+
+// push() -> agrega el elemento al final del array
+diasDeSemana.push('Octes');
+console.log(diasDeSemana);
+
+// pop() -> elimina el último elemento del array
+console.log(diasDeSemana.pop());
+console.log(diasDeSemana);
