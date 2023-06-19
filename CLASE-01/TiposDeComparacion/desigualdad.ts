3f418eab9d7107905be1e24f1051fded9b7f51f8
Jose Antonio Burgos <jose_7892@hotmail.com>
2023-06-18 21:59:59 -0300

    Igualdad, desigualdad y negacion

---
+++ CLASE-01/TiposDeComparacion/desigualdad.ts
@@ -0,0 +1,13 @@
+let a=1, b='01';
+
+// DESIGUALDAD ESTRICTA
+//console.log(a !== b); // true
+console.log(undefined !== null); // true
+//console.log(NaN !== NaN); // true
+console.log(Infinity !== Infinity); // false
+
+// DESIGUALDAD ABSTRACTA
+//console.log(a != b); // false
+console.log(undefined != null); // false
+//console.log(NaN != NaN); // true
+console.log(Infinity != Infinity); // false
---
+++ CLASE-01/TiposDeComparacion/igualdad.ts
@@ -0,0 +1,13 @@
+let a = 1, b = '01';
+
+// ABSTRACTA
+//console.log(a == b); // true
+console.log(undefined == null); // true
+//console.log(NaN == NaN); // false
+console.log(Infinity == Infinity); // true
+
+// ESTRICTA
+//console.log(a === b); // false
+console.log(undefined === null); // false
+//console.log(NaN === NaN); // false
+console.log(Infinity === Infinity); // true 
