37678c87dba0306322c74c65b922cff7132dad3c
Jose Antonio Burgos <jose_7892@hotmail.com>
2023-06-18 23:24:03 -0300

    Ejemplo de for con if-else

--- CLASE-01/Ciclos/for.ts
+++ CLASE-01/Ciclos/for.ts
@@ -1,3 +1,22 @@
-for (let i = 1; i <= 10; i++) {
+/*for (let i = 1; i <= 10; i++) {
     console.log(i);
+}*/
+
+// Ejemplo usando for, if-else
+for (let diaDeSemana = 1; diaDeSemana <= 7; diaDeSemana++) {
+    if (diaDeSemana === 1) {
+        console.log("Hoy es Lunes");
+    } else if (diaDeSemana === 2) {
+        console.log("Hoy es Martes");
+    } else if (diaDeSemana === 3) {
+        console.log("Hoy es Miércoles");
+    } else if (diaDeSemana === 4) {
+        console.log("Hoy es Jueves");
+    } else if (diaDeSemana === 5) {
+        console.log("Hoy es Viernes");
+    } else if (diaDeSemana === 6) {
+        console.log("Hoy es Sábado");
+    } else {
+        console.log("Hoy es Domingo");
+    }
 }
