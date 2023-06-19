e0e634f59d7e0a579c8186e2ccde5d7d05d46ce7
Jose Antonio Burgos <jose_7892@hotmail.com>
2023-06-18 22:30:51 -0300

    Condicional if-else-if

--- CLASE-01/Condicionales/if-else.ts
+++ CLASE-01/Condicionales/if-else.ts
@@ -1,8 +1,10 @@
-const numero = 7;
+const numero = 107;
 
 if (numero % 2 == 0) {
     console.log("El número es par");
-}else{
-    console.log("El número es impar");
+} else if (numero < 10) {
+    console.log("El número es impar y menor que 10");
+} else {
+    console.log("El número es impar y mayor que 10");
 }
 console.log("El programa está terminado");
