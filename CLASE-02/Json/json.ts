e9c4d6d5dd931cbda237a40271d70b09112626b2
Jose Antonio Burgos <jose_7892@hotmail.com>
2023-06-20 16:36:34 -0300

    Json y ejemplo

---
+++ CLASE-02/Json/json.ts
@@ -0,0 +1,17 @@
+// json -> utilizados para representar objetos. Se basan en el concepto de clave-valor
+
+// Ejemplo
+let perro = {
+    "nombre": "Rufo",
+    "raza": "Golden Retriever",
+    "edad": 5,
+    "comidasFavoritas": ["Solomillo", "Atun"],
+    "gustaJugar": true,
+    "vacunas": {
+        "rabia": true,
+        "moquillo_canino": true,
+        "parvo": true,
+    }
+}
+
+console.log(perro);
