6f1263d7cb718c2818c665f3503631b942bd1c35
Jose Antonio Burgos <jose_7892@hotmail.com>
2023-06-20 03:05:02 -0300

    Tipo de dato void

---
+++ CLASE-02/TiposEspecialesDeDatos/void.ts
@@ -0,0 +1,7 @@
+// void -> es una forma de representar el vacío, no indica ni undefined ni null
+// Se utiliza en funciones para indicar que no retorna ningún valor
+function funcionSinRetorno(): void {
+    console.log("Esta función solo muestra este mensaje sin retornar valor alguno");
+}
+
+funcionSinRetorno();
