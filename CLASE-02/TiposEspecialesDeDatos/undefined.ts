8ed1ddfe3e2a45d882602713ecc908f175ca3b91
Jose Antonio Burgos <jose_7892@hotmail.com>
2023-06-20 02:50:29 -0300

    Tipo de dato undefined

---
+++ CLASE-02/TiposEspecialesDeDatos/undefined.ts
@@ -0,0 +1,5 @@
+// undefined -> es el 'valor' que toma una variable no inicializada
+let queNumeroEsEste: number;
+
+console.log(queNumeroEsEste); // Da error xq el tipo undefined no coincide con tipo number
+
