cf079bf4ca48ff65980ce4c5d2eea32c0c9a9f55
Jose Antonio Burgos <jose_7892@hotmail.com>
2023-06-20 17:47:01 -0300

    Aplicando interfaz

---
+++ CLASE-02/Interfaces/ejemploInterfaz.ts
@@ -0,0 +1,42 @@
+interface IPerro {
+    nombre: string;
+    raza: string;
+    edad: number;
+    peso: number;
+    color: string;
+    comidasFavoritas: string[];
+    gustaJugar: boolean;
+    vacunas?: {     // el signo ? indica que es opcional
+        rabia: boolean;
+        moquilloCanino: boolean;
+        parvo: boolean;
+    }
+}
+
+// Aplicando a perro1 interfaz IPerro
+const perro1: IPerro = {
+    "nombre": "Rufo",
+    "raza": "Golden Retriever",
+    "edad": 5,
+    "peso": 23,
+    "color": "marron",
+    "comidasFavoritas": ["Solomillo", "Atun"],
+    "gustaJugar": true,
+    "vacunas": {
+        "rabia": true,
+        "moquilloCanino": true,
+        "parvo": true,
+    }
+}
+
+// Aplicando a perro2 interfaz IPerro (vacunas es opcional por lo que al no tener vacunas no genera error)
+const perro2: IPerro = {
+    "nombre": "Rufo",
+    "raza": "Golden Retriever",
+    "edad": 5,
+    "peso": 45,
+    "color": "marron",
+    "comidasFavoritas": ["Solomillo", "Atun"],
+    "gustaJugar": true
+}
+
